8caa901b2fe4c6e1112535833c0707a68c19b9f6
Dan Colmenares <dacolmenares93@gmail.com>
2022-09-06 14:34:49 -0500

    Feat: More specific Docs

--- utils/getRelativeTime.ts
+++ utils/getRelativeTime.ts
@@ -4,6 +4,8 @@ dayjs.extend(relativeTime);
 
 /**
  * Returns the string of relative time from now, using dayjs library and its plugin: RelativeTime.
+ * @param {string} date The date to get the relative time that has passed between then and today.
+ * @return {string} String to indicate the exact time that has passed.
  */
 export const getRelativeTime = (date: string): string => {
   return dayjs(date).fromNow();
